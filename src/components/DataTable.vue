a5466dacb89c3f1544af2e9a2663fa3e39da61cc
dokechin <ttatsumi@ra2.so-net.ne.jp>
2018-04-24 08:04:08 +0900

    chages test simple

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,47 +1,8 @@
 <template>
-  <v-container fluid>
-    <v-layout row wrap mb-2>
-      <v-flex xs6>
-        <v-text-field
-            append-icon="search"
-            :label="search"
-            single-line
-            hide-details
-            v-model="search">
-        </v-text-field>
-      </v-flex>
-      <v-flex xs5 />
-      <v-flex xs1>
-        <v-btn id="new" @click.stop="dialog = true" color="primary" dark slot="activator" class="mb-2">new</v-btn>
-      </v-flex>
-    </v-layout>
-    <v-dialog v-model="dialog" max-width="500px">
-      <v-card>
-        <v-form v-model="formValid" ref="form">
-          <v-card-title>
-            <span class="headline">title</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex xs12 sm6 md6>
-                  <v-text-field id="shopName" :label="name" :rules="nameRules" v-model="editedItem.name" required></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md6>
-                  <v-checkbox id="taxIncluded" :label="pretax" v-model="editedItem.taxIncluded"></v-checkbox>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn id="cancel" color="blue darken-1" flat @click.native="close">cancel</v-btn>
-            <v-btn id="save" color="blue darken-1" flat @click.native="save" :disabled="!formValid">save</v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-dialog>
-    <v-data-table :headers="headers" :search="search" :pagination.sync="pagination" :items="shops" class="elevation-1">
+  <v-container>
+    <v-text-field id="shopName" v-model="editedItem.name" required></v-text-field>
+    <v-btn id="save" color="blue darken-1" flat @click.native="save">save</v-btn>
+    <v-data-table :headers="headers" :pagination.sync="pagination" :items="shops" class="elevation-1">
       <template slot="items" slot-scope="props">
           <td>{{ props.item.name }}</td>
       </template>
@@ -62,42 +23,21 @@ export default {
       pagination: {
         sortBy: 'name'
       },
-      search: '',
-      formValid: false,
-      dialog: false,
       headers: [
         {
           text: 'name',
           align: 'left',
           value: 'name'
-        },
-        { text: 'tax', value: 'taxIncluded' },
-        { text: 'action', value: 'name', sortable: false }
+        }
       ],
-      editedIndex: -1,
-      editedItem: {name: '', taxIncluded: false},
-      defaultItem: {name: '', taxIncluded: false},
-      updateValid: true,
-      nameRules: [
-        (v) => !!v || 'empty',
-        (v) => !(v.length > 30) || 'length_over_30'
-      ]
+      editedItem: {name: ''},
+      defaultItem: {name: ''}
     }
   },
   methods: {
-    editItem (shop) {
-      this.editedIndex = this.shops.indexOf(shop)
-      this.editedItem = Object.assign({}, shop)
-      this.dialog = true
-    },
-    deleteItem (shop) {
-      this.$store.commit('removeShop', shop)
-    },
     close () {
-      this.dialog = false
       setTimeout(() => {
         this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
       }, 300)
     },
     save () {
--- test/unit/specs/DataTable.spec.js
+++ test/unit/specs/DataTable.spec.js
@@ -6,61 +6,22 @@ import { store } from '../../../src/store/store.js'
 
 Vue.use(Vuetify)
 
-function _clickNewButton(wrapper){
-  wrapper.find('button#new')[0].trigger('click')  
-}
-
-function _clickEditButton(wrapper){
-  wrapper.find('button')[1].trigger('click')
-}
-
-function _clickZeikomiCheck(wrapper){
-  const flag = wrapper.find('.input-group--selection-controls__ripple')[0]
-  flag.trigger('click')
-}
-
-function _inputName(wrapper, name){
-  const inputShopName = wrapper.find('input#shopName')[0]
-  inputShopName.trigger('focus')
-  inputShopName.element.value = name
-  inputShopName.trigger('input')
-}
-
-function _clickSave(wrapper){
-  wrapper.find('button#save')[0].trigger('click')
-}
-
 describe('Shop.vue', () => {
   it('adds a new shop', async () => {
     // build component
     const wrapper = mount(DataTable, {store})
-    expect(wrapper.vm.dialog).toBe(false)
-    _clickNewButton(wrapper)
-    expect(wrapper.vm.dialog).toBe(true)
-    _clickZeikomiCheck(wrapper)
-    _inputName(wrapper, 'AEON')
+    const inputShopName = wrapper.find('input#shopName')[0]
+    inputShopName.trigger('focus')
+    inputShopName.element.value = 'AEON'
+    inputShopName.trigger('input')
     await wrapper.vm.$nextTick()
-    expect(wrapper.vm.formValid).toBe(true)
-    _clickSave(wrapper)
+    wrapper.find('button#save')[0].trigger('click')
     await wrapper.vm.$nextTick()
-    expect(store.state.items[0].name).toBe('AEON')
     expect(wrapper.html()).toContain('AEON')
-
+  })
+  it('uses same store', () => {
     // build component
     const wrapper2 = mount(DataTable, {store})
-    console.log(store.state.items[0].name)
-    console.log(wrapper2.html())
-    expect(wrapper2.vm.dialog).toBe(false)
-    _clickEditButton(wrapper2)
-    expect(wrapper2.vm.dialog).toBe(true)
-    _clickZeikomiCheck(wrapper2)
-    _inputName(wrapper2, 'SAYYOU')
-    await wrapper2.vm.$nextTick()
-    expect(wrapper2.vm.formValid).toBe(true)
-    _clickSave(wrapper2)
-    await wrapper2.vm.$nextTick()
-    expect(store.state.items[0].name).toBe('SAYYOU')
-    expect(store.state.items[0].taxIncluded).toBe(false)
-    expect(wrapper2.html()).toContain('SAYYOU')
+    expect(wrapper2.html()).toContain('AEON')
   })
 })
